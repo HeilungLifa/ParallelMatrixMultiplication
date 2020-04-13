4187897085bbcd0e364538eee72f560858bb8296
HeilungLifa <Black8192water@gmail.com>
2020-04-13 14:24:23 +0300

    Initital commit

---
+++ Parallel_processing_cpp/Parallel_processing_cpp.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <thread>
+#include <mutex>
+#include <fstream>
+
+using namespace std;
+
+#define n 500
+int repeat = 100;
+
+string fileName = "result.txt";
+
+double multiplier_1[n][n];
+double multiplier_2[n][n];
+double product[n][n];
+
+int threadCount = 0;
+thread* hThreads;
+
+HANDLE hStart = CreateEvent(NULL, true, false, NULL);
+
+void ResetToZero(double(&m)[n][n])
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			m[i][j] = 0;
+		}
+	}
+}
+
+void MatrixMultiplication(int index)
+{
+	WaitForSingleObject(hStart, INFINITE);
+	for (int i = index; i < n; i += threadCount) {
+		for (int j = 0; j < n; j++){
+			for (int k = 0; k < n; k++) {
+				product[i][j] += multiplier_1[i][k] * multiplier_2[k][j];
+			}
+		}
+	}
+}
+
+int main() {
+	srand((int)time(0));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			multiplier_1[i][j] = 10 * (double)rand() / RAND_MAX;
+			multiplier_2[i][j] = 10 * (double)rand() / RAND_MAX;
+		}
+	}
+
+	printf("Threads count: ");
+	cin >> threadCount;
+
+	double sumTime = 0;
+	double seconds = 0;
+	ofstream fout(fileName);
+	for (int k = 0; k < repeat; k++)
+	{
+		ResetToZero(product);
+		hThreads = new thread[threadCount];
+
+		for (int i = 0; i < threadCount; i++) {
+			hThreads[i] = thread(MatrixMultiplication, i);
+		}
+		
+		clock_t start = clock();
+		
+		SetEvent(hStart);
+		for (int i = 0; i < threadCount; i++) {
+			hThreads[i].join();
+		}
+		
+		seconds = ((double)clock() - (double)start) / 1000.0;
+		sumTime += seconds;
+		fout << seconds << '\n';
+
+		delete[] hThreads;
+		ResetEvent(hStart);
+	}
+	fout.close();
+	printf("\nthreads: %d\ntime: %f", threadCount, (sumTime / repeat));
+	
+	char* filename = new char[fileName.length() + 1];
+	strcpy(filename, fileName.c_str());
+	system(filename);
+	
+	system("pause");
+	return 0;
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Parallel_processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch time_counter = new Stopwatch();
+            Random rnd = new Random();
+            List<Thread> threads = new List<Thread>();
+            EventWaitHandle wait_handle = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+            int matrix_size = 300;
+            int repeat = 100;
+            long time = 0;
+            string timeToFile = "";
+            
+            double[,] addendum = new double[matrix_size, matrix_size];
+            double[,] _addendum = new double[matrix_size, matrix_size];
+            double[,] amount = new double[matrix_size, matrix_size];
+
+            for (int i = 0; i < matrix_size; i++)
+            {
+                for (int j = 0; j < matrix_size; j++)
+                {
+                    addendum[i, j] = rnd.NextDouble() * 10;
+                    _addendum[i, j] = rnd.NextDouble() * 10;
+                }
+            }
+
+            Console.Write("Threads count: ");
+            int.TryParse(Console.ReadLine(), out int threads_count);
+
+            
+            for (int k = 0; k < repeat; k++)
+            {
+                amount = new double[matrix_size, matrix_size];
+                for (int t = 0; t < threads_count; t++)
+                {
+                    Thread thread = new Thread(() => 
+                    {
+                        wait_handle.WaitOne();
+                        for (int i = t; i < matrix_size; i += threads_count)
+                        {
+                            for (int j = 0; j < matrix_size; j++)
+                            {
+                                for (int l = 0; l < matrix_size; l++)
+                                {
+                                    amount[i, j] += addendum[i, l] * _addendum[l, j];
+                                }
+                            }
+                        }
+                    });
+                    threads.Add(thread);
+                    thread.Start();
+                }
+
+                time_counter.Restart();
+                wait_handle.Set();
+                for (int i = 0; i < threads_count; i++)
+                {
+                    threads[i].Join();
+                }
+                wait_handle.Reset();
+                time_counter.Stop();
+                time += time_counter.ElapsedMilliseconds;
+                timeToFile += (time_counter.ElapsedMilliseconds / 1000.0).ToString() + Environment.NewLine;
+            }
+            Console.WriteLine($"threads: {threads_count}{Environment.NewLine}time: {time / repeat}");
+            File.WriteAllText("results.txt", timeToFile);
+            Process.Start("notepad", "results.txt");
+            Console.ReadKey();
+        }
+    }
+}
